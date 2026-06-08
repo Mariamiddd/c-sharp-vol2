6791376dfe0553adb9df0d638f2eb0f0e9731738
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-06-08 21:04:58 +0400

    /

--- davaleba9/MusicalInstrument.cs
+++ davaleba9/MusicalInstrument.cs
@@ -1,14 +1,19 @@
 ﻿namespace davaleba9
 {
+    // ვქმნით base კლასს, რომელიც აბსტრაქტულია და ვქმნით სხვადასხვა ფაილებში 4 მუსიკალურ
+    // ინსტრუმენტს, რომელსაც ვაძლევთ ამ კლასის მეთოდებს რომელზეც ოვერრაიდ არის შესაძლებელი
     internal abstract class MusicalInstrument
     {
 
         public string Name { get; set; }
 
+        // კონსტრუქტორი, რომელიც იღებს ინსტრუმენტის სახელს
         public MusicalInstrument(string name)
         {
             Name = name;
         }
+
+        // აბსტრაქტული მეთოდები რომლებსაც ოვერრაიდი ექნება მემკვიდრე კლასებში
         public abstract void Sound();
         public abstract void Show();
         public abstract void Desc();
