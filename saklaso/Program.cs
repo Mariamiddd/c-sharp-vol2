c1a4a7a466978920c310b3ffa736bf6d6dcaa8a3
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-06-15 22:37:21 +0400

    commit

---
+++ saklaso/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+namespace saklaso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"../../../data.txt";
+
+            string[] lines = File.ReadAllLines(path);
+
+            Student[] users = new Student[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] parts = lines[i].Split(',');
+                Console.WriteLine(parts[2]);
+                users[i] = new Student();
+                users[i].FirstName = parts[0];
+
+                users[i].Age = int.Parse(parts[2]);
+                users[i].Point = int.Parse(parts[5]);
+            }
+
+            #region davaleba1 
+            //ვიპოვოთ ისეთი სტუდენტი რომელსაც აქვს ყველაზე დაბალი ქულა
+
+            //ავტტომატურად ვაცხადებთ პირველ სტუდენტს ყველაზე დაბალქულიანად
+            Student lowestPointStudent = users[0];
+
+            for (int i = 1; i < users.Length; i++)
+            {
+                if (users[i].Point < lowestPointStudent.Point)
+                {
+                    lowestPointStudent = users[i];
+                }
+            }
+            Console.WriteLine($"{lowestPointStudent.FirstName} has lowest score : {lowestPointStudent.Point}");
+
+
+            #endregion
+
+
+            #region davaleba 2
+            //ვიპოვოთ ისეთი სტუდენტი რომელიც ყველაზე დიდია ასაკით
+
+            Student oldestStudent = users[0];
+
+            for (int i = 1; i < users.Length; i++)
+            {
+                if (users[i].Age > oldestStudent.Age)
+                {
+                    oldestStudent = users[i];
+                }
+
+
+            }
+            Console.WriteLine($"{oldestStudent.FirstName} is oldest. Age : {oldestStudent.Age}");
+
+            #endregion
+
+            #region davaleba 3         
+            //ვიპოვოთ ყველა სტუდენტის საშუალო ქულა
+            //pirvelad udris 0 jami
+            double total = 0;
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                total += users[i].Point; //emateba for loop is sashualebiT titoeuli jams 
+            }
+            double averagePoint = total / users.Length;
+            Console.WriteLine($"average score of students is: {averagePoint}");
+
+            #endregion
+
+            #region  davaleba 4
+
+            // დაასორტირეთ სტუდენტების მასივი
+            //ასაკის მიხედვით დასორტირება
+            //gare cikliT vakontrolebt ramdenjer gadauvlis shida masivs
+            for (int i = 0; i < users.Length - 1; i++)
+            {
+                //gverdi gverd adarebs ricxvebs
+                for (int j = 0; j < users.Length - 1 - i; j++)
+                {
+                    if (users[j] > users[j + 1])
+                    {
+                        Student temporarCvladi = users[j];
+                        users[j] = users[j + 1];
+                        users[j + 1] = temporarCvladi;
+                    }
+                }
+            }
+
+            //გამოვიტანთ დასორტირებულ სიას
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine($"{users[i].FirstName} - Age: {users[i].Age}, Point: {users[i].Point}");
+            }
+
+            #endregion
+        }
+
+    }
+}
---
+++ saklaso/Students.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace saklaso
+{
+    internal class Student
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public int Point { get; set; }
+
+
+        //ოპერატორის გადატვირთვა >ზე
+        public static bool operator >(Student s1, Student s2)
+        {
+            return s1.Age > s2.Age;
+        }
+
+        // გადატვირთვა < ოპერატორზე
+        public static bool operator <(Student s1, Student s2)
+        {
+            return s1.Age < s2.Age;
+        }
+
+        //ოპერატორების გადათვირთვა +
+        public static  double operator + (Student s1, Student s2)
+        {
+            return s1.Point * s2.Point;
+        }
+
+        // gadatvirtva / gayofaze
+        public static double operator /(Student s, int raodenoba)
+        {
+            return (double)s.Point / raodenoba;
+        }
+
+
+
+        //ვიპოვოთ ისეთი სტუდენტი რომელსაც აქვს ყველაზე დაბალი ქულა
+        //ვიპოვოთ ისეთი სტუდენტი რომელიც ყველაზე დიდია ასაკით
+        //ვიპოვოთ ყველა სტუდენტის საშუალო ქულა
+        // დაასორტირეთ სტუდენტების მასივი
+    }
+}
