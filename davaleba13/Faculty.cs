a1a357a446611893b634a615e3ad3939b24e045e
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-06-18 22:53:09 +0400

    ცომმიტ

---
+++ davaleba13/Faculty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace davaleba13
+{
+    //faculty enam class სადაც enum სადაც განსაზვღული კონსტანტები გვაქვს 
+    public enum Faculty
+    {
+        IT,
+        Business,
+        Design,
+        Medicine
+    }
+}
---
+++ davaleba13/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace davaleba13
+{
+    public class Student
+    {
+        //student class with properties
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public byte Age { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public double GPA { get; set; }
+        public Faculty Faculty { get; set; }
+
+        //კონსტრუქტორი მოგვიანებით ობიქტების შესაქმნელად
+
+        public Student(string name, string lastName, byte age, string email, string phone, double gpa, Faculty faculty)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            Email = email;
+            Phone = phone;
+            GPA = gpa;
+            Faculty = faculty;
+
+
+        }
+
+
+    }
+}
