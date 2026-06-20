2aea3dd2976b2124076703f41e204f4760d15b76
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-06-21 00:57:06 +0400

    commit

--- davaleba13/Program.cs
+++ davaleba13/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+
 
 namespace davaleba13
 {
@@ -50,7 +51,7 @@ namespace davaleba13
                         {
                             if (s != null)
                             {
-                                Console.WriteLine($" {s.Name} {s.LastName}, Age: {s.Age}, Faculty: {s.Faculty}, GPA: {s.GPA}");
+                                Console.WriteLine($"{s.Name} {s.LastName}, Age: {s.Age}, Faculty: {s.Faculty}, GPA: {s.GPA}");
                             }
                         }
                         break;
@@ -61,7 +62,7 @@ namespace davaleba13
 
                         foreach (Student s in students)
                         {
-                            if (s!= null && s.GPA > bestGpa)
+                            if (s != null && s.GPA > bestGpa)
                             {
                                 bestGpa = s.GPA;
                             }
@@ -89,7 +90,7 @@ namespace davaleba13
                         double totalGpaScore = 0;
                         int studentAmount = 0;
 
-                        for (int i = 0; i< students.Length; i++)
+                        for (int i = 0; i < students.Length; i++)
                         {
                             if (students[i] != null)
                             {
@@ -97,12 +98,146 @@ namespace davaleba13
                                 studentAmount++;
                             }
                         }
-                        if(studentAmount> 0) //vamowmebT rom nulze gayofa ar moxdes
+                        if (studentAmount > 0) //vamowmebT rom nulze gayofa ar moxdes
                         {
                             double averageGpa = totalGpaScore / studentAmount;
                             Console.WriteLine($"students average score is: {Math.Round(averageGpa, 3)}");
                         }
                         break;
+                    //პუნქტი 4: გვარითს სტუდენტის მოძებნა
+                    case "4":
+                        Console.Write("enter lastName:");
+                        //momxmareblis sheyvanili gvari
+                        string searchedLastname = Console.ReadLine();
+                        string formattedInput = searchedLastname.Trim().ToLower();
+
+                        bool isFound = false;
+                        foreach (Student s in students)
+                        {
+                            if (s != null)
+                            {
+                                string studentLastName = s.LastName.ToLower();
+                                if (studentLastName.Contains(formattedInput))
+                                {
+                                    Console.WriteLine($"founded: {s.Name} {s.LastName}, Age: {s.Age}, Faculty: {s.Faculty}, GPA: {s.GPA}");
+                                    isFound = true;
+                                }
+                            }
+                        }
+                        if (!isFound)
+                        {
+                            Console.WriteLine("student with that lastname doesnt exist");
+                        }
+                        break;
+
+                    //პუნქტი 5: gpa ქულების მიხედვით დალაგება
+
+                    case "5":
+                        //bubble sort
+                        for (int i = 0; i < students.Length - 1; i++)
+                        {
+                            for (int j = 0; j < students.Length - 1 - i; j++)
+                            {
+                                if (students[j] != null && students[j + 1] != null)
+                                {
+                                    if (students[j].GPA < students[j + 1].GPA)
+                                {
+                                    Student temp = students[j];
+                                    students[j] = students[j + 1];
+                                    students[j + 1] = temp;
+                                }
+                            }
+                            else if (students[j] == null && students[j + 1] != null)
+                            {
+                                Student temp = students[j];
+                                students[j] = students[j + 1];
+                                students[j + 1] = temp;
+                            }
+                        }
+                }
+                        foreach (Student s in students)
+                        {
+                            if (s != null)
+                            {
+                                Console.WriteLine($"{s.Name} {s.LastName} | GPA: {s.GPA}");
+                            }
+                        }
+                        break;
+                        //პუნქტი 6: სტუდენტის დამატება
+                    case "6":
+                        try
+                        {
+                            Console.Write("Enter NAme: ");
+                            string newName = Console.ReadLine();
+
+                            Console.Write("enter lastname: ");
+                            string newlastName = Console.ReadLine();
+
+                            Console.Write("enter age(minimal age should be 17):");
+                            int newAge = byte.Parse(Console.ReadLine());
+                            if( newAge < 17 )
+                            {
+                                Console.WriteLine("Student should be older than 16 yars old!");
+                                break;
+                            }
+                            Console.Write("enter Email");
+                            string newEmail = Console.ReadLine();
+                            if(!newEmail.Contains("@"))
+                            {
+                                Console.WriteLine("try again! email should consist @!");
+                                break;
+                            }
+
+                            Console.Write("Enter phone  number:");
+                            string newPhone = Console.ReadLine();
+
+                            Console.Write("enter gpa(0-100 diapazonshi):");
+                            double newGpa = double.Parse(Console.ReadLine());
+                            if(newGpa < 0 || newGpa > 100)
+                            {
+                                Console.WriteLine("try again: gpa should be 0-100!");
+                                break;
+                            }
+
+                            Console.Write("choose faculty (0: IT, 1: Business, 2: Design, 3: Medicine):");
+                            int facultyChoice = int.Parse(Console.ReadLine());
+                            Faculty newFaculty = (Faculty)facultyChoice;
+
+                            //vzrdit masivs 1it
+                            Array.Resize(ref students, students.Length + 1);
+
+                            students[students.Length - 1] = new Student(newName, newlastName, (byte)newAge, newEmail, newPhone, newGpa, newFaculty);
+                            Console.WriteLine("ახალი სტუდენტი წარმატებით დაემატა!");
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("შეცდომა: გთხოვთ შეიყვანოთ სწორი ფორმატის მონაცემები (მაგ: ასაკი და GPA უნდა იყოს რიცხვი)!");
+                        }
+                        break;
+
+                    case "7":
+                        Console.Write("Enter Email to delete the student: ");
+                        string inputEmail = Console.ReadLine().Trim().ToLower();
+                        bool isStudentDeleted = false;
+
+                        //vamowmebt for loop it titoeul students
+
+                        for (int i = 0; i < students.Length; i++)
+                        {
+                            if (students[i] != null && students[i].Email.ToLower() == inputEmail)
+                            {
+                                Console.WriteLine($"Student {students[i].Name} {students[i].LastName} has been deleted.");
+                                students[i] = null;
+                                isStudentDeleted = true;
+                                break;
+                            }
+                        }
+                        if(!isStudentDeleted)
+                        {
+                            Console.WriteLine("student with this mail not found!");
+                        }
+                        break;
+
 
                     // პროგრამიდან გასვლა როდესაც bool ვანიჭებთ false მნიშვნელობას
                     case "8":
