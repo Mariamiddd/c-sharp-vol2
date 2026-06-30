070ee424c76f54c9747281e85a09a26ed7f817ac
Mariamiddd <mariami.tediashvili9076@hum.tsu.edu.ge>
2026-06-30 19:48:50 +0400

    delegates

---
+++ davaleba15/Algorithms.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Serialization.Metadata;
+
+namespace davaleba15
+{
+    public static class Algorithms
+    {
+        //where 
+        public static IEnumerable<T> Filter<T>(
+            this IEnumerable<T> filteredData, Func<T, bool> predicate)
+        {
+            foreach (T item in filteredData)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+
+                }
+            }
+        }
+
+        //////////////////////////////////////////////////////////////
+
+        //orderby 
+        public static IEnumerable<T> OrderedData<T, TValue>(this IEnumerable<T> data, Func<T, TValue> keySelector)
+            where TValue : IComparable<TValue>
+        {
+            List<T> list = new List<T>(data);
+
+            // usin bubble sort
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count - 1 - i; j++)
+                {
+                    TValue value1 = keySelector(list[j]);
+                    TValue value2 = keySelector(list[j + 1]);
+
+                    if (value1.CompareTo(value2) > 0)
+                    {
+                        //  swaping places
+                        T temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+
+            foreach (T item in list)
+            {
+                yield return item;
+            }
+        }
+
+        //3 first
+        public static T FirstElement<T>(this IEnumerable<T> data)
+        {
+            foreach (T item in data)
+            {
+                return item;
+            }
+            return default;
+
+        }
+
+
+        //4firstor defaulr
+        public static T FirstOrDefault<T>(this IEnumerable<T> data)
+        {
+            foreach (T item in data)
+            {
+                return item;
+            }
+            return default(T);
+        }
+
+        //5 single element or exception
+        public static T CustomSingle<T>(this IEnumerable<T> data, Func<T, bool> predicate)
+        {
+            T result = default(T);
+            bool found = false;
+
+            foreach (T item in data)
+            {
+                if (predicate(item))
+                {
+                    if (found) throw new InvalidOperationException("found more than 1 element");
+                    result = item;
+                    found = true;
+                }
+            }
+
+            if (!found) 
+                throw new InvalidOperationException("element wasnt found");
+            return result;
+        }
+
+        //6. single or default
+
+        public static T CustomSingleOrDefault<T>(this IEnumerable<T> data, Func<T, bool> predicate)
+        {
+            T result = default(T);
+            bool found = false;
+
+            foreach (T item in data)
+            {
+                if (predicate(item))
+                {
+                    if (found) throw new InvalidOperationException("foud more than 1 element");
+                    result = item;
+                    found = true;
+                }
+            }
+
+            return result; // if not found resultt will be defalt anu 0
+        }
+
+        //7. any
+        public static bool CustomAny<T>(this IEnumerable<T> data, Func<T, bool> predicate)
+        {
+            foreach (T item in data)
+            {
+                if (predicate(item))
+                    return true;
+            }
+            return false;
+        }
+
+        //8. all
+        public static bool CustomAll<T>(this IEnumerable<T> data, Func<T, bool> predicate)
+        {
+            foreach(T item in data)
+            {
+                if (!predicate(item)) 
+                    return false;  //if one isnt true it will rerun false
+            }
+            return true; //else returns true
+        }
+
+
+        //9. customcount
+        public static int CustomCount<T>(this IEnumerable<T> data, Func<T, bool> predicate = null)
+        {
+            int count = 0;
+            foreach(T item in data)
+            {
+                if(predicate == null || predicate(item))
+                {
+                    count++;
+                }
+
+            }
+            return count;
+        }
+
+        //10 distinct
+
+        public static IEnumerable<T> CustomDistinct<T>(this IEnumerable<T> data)
+        {
+            List<T> allElements = new List<T>();
+
+            foreach (T item in data)
+            {
+                if (!allElements.Contains(item))
+                {
+                    allElements.Add(item); 
+                    yield return item;      
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ davaleba15/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace davaleba15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //testing filter algorithm
+            List<int> numbers = new List<int> { 1, 2, 567, -123, -1,5, -32, 7843, 32, 64 };
+
+            // using filter on the num list
+            IEnumerable<int> positiveNums = numbers.Filter(n => n > 0);
+
+            Console.WriteLine("1. Numbers > 0 are:");
+            foreach (int i in positiveNums)
+            {
+                Console.WriteLine(i);
+            }
+
+            //2 ordered
+            Console.WriteLine("2. ordered list: ");
+            IEnumerable<int> orderedList = numbers.OrderedData(n => n);
+            foreach(int i in orderedList)
+                {  Console.WriteLine(i); }
+
+            //3 first
+            Console.WriteLine("3. first element of the list:");
+            int first = numbers.FirstElement();
+            Console.WriteLine(first);
+
+            // 4 first or default 
+            Console.WriteLine("4. first or default:");
+            List<int> firstorD = new List<int> {  };
+            int firstElD = firstorD.FirstElement();
+            //with empty list
+            Console.WriteLine(firstElD);
+            int firstEl = numbers.FirstElement();
+            //with list with members/nums
+            Console.WriteLine(firstEl);
+
+            //5 Single
+            Console.WriteLine("5. single or default:");
+            int single = numbers.CustomSingle(n => n == 5);
+            Console.WriteLine(single); 
+
+            //6. SingleOrDefault
+            Console.WriteLine("6. single or default:");
+            int SingleOrDefault = numbers.SingleOrDefault(n => n == 6);
+            Console.WriteLine(SingleOrDefault);
+
+            //7. custum any
+            Console.WriteLine("7. ANY");
+            bool hasNum = numbers.CustomAny(n => n == 5);
+            Console.WriteLine(hasNum);
+
+            //8.ALL
+            Console.WriteLine("8. ALL");
+            bool IsAllTrue = numbers.CustomAll(n => n > 0);
+            Console.WriteLine(IsAllTrue);
+
+            //9. count
+            Console.WriteLine("9. customcount");
+            int CountTotal = numbers.CustomCount(n => n < 0);
+            Console.WriteLine(CountTotal);
+
+            //10.distinct
+            Console.WriteLine("10. distinct");
+            IEnumerable<int> noDuplicates = numbers.CustomDistinct();
+
+            foreach (int i in noDuplicates)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
